50fb8fb2c76944cb7883d03f976c14f5e3a7757c
jhyun94 <hyunjonathan@gmail.com>
2017-03-22 16:11:48 -0500

    titles are clickable and have ids attached to them

--- application.js
+++ application.js
@@ -2,21 +2,25 @@ $( document ).ready(function() {
     $("form").on("submit", function(){
       event.preventDefault();
       var data = $("#search").val();
-
-      $.ajax({
-        method: "GET",
-        url: "http://www.omdbapi.com/?s=" + data,
-      }).done(function(response){
-        displayResult(response);
-      })
+      if (data != 0){
+        $.ajax({
+          method: "GET",
+          url: "http://www.omdbapi.com/?s=" + data,
+        }).done(function(response){
+          displayResult(response);
+        })
+      }
     })
+
 
 
 
 
   function displayResult(response) {
     for(var i = 0; i <10; i++){
-      $(".list").append('<li>' + response["Search"][i]["Title"]);
+      $(".list").append('<li><p class="clickable">' + response["Search"][i]["Title"] + '</p>'
+        + '<input type="hidden" class="imdbID" value=' + response["Search"][i]["imdbID"] 
+        + '></li>');
     }
   }
 });
