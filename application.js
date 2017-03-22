8d2456da7bfd9ba36ff24514a2184e416df53346
jhyun94 <hyunjonathan@gmail.com>
2017-03-22 15:12:16 -0500

    able to get data from imdb api

--- application.js
+++ application.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $("form").on("submit", function(){
+      event.preventDefault();
+      var data = $("#search").val();
+
+      $.ajax({
+        method: "GET",
+        url: "http://www.omdbapi.com/?s=" + data,
+      }).done(function(response){
+        displayResult(response);
+      })
+    })
+    test();
 });
+
